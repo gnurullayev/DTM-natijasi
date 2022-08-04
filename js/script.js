5237ce17cf50af549396ed42dece0a0d73e14212
gnurullayev <nurullayevgayrat73@gmail.com>
2022-08-04 22:23:53 +0500

    project finished

---
+++ js/script.js
@@ -0,0 +1,57 @@
+var elForm = document.querySelector(".form");
+var elFormInput = elForm.querySelector(".form-input");
+var elFormSelect = elForm.querySelector(".select-form");
+var elFormCheck = elForm.querySelector(".check-form");
+var elFormbtn = elForm.querySelector(".form-btn");
+var answerText = document.querySelectorAll(".text");
+
+
+
+
+elForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    
+    var checkboxValue = Number(elFormCheck.value) ;
+    if(elFormCheck.checked) {
+        checkboxValue = 0.5
+    }
+
+    var inputValue = Number(elFormInput.value);
+    var selectValue = Number(elFormSelect.value) ;
+
+    var discount = inputValue*checkboxValue;
+    var maxBall = inputValue + discount;
+
+    if(inputValue >= selectValue + 20 || maxBall >= selectValue + 20) {
+        answerText[0].textContent = "Tabiriklayman siz talabalikka grand asosida qabul qilindingiz "
+    }
+    else if (inputValue >= selectValue && inputValue < selectValue + 20 || maxBall >= selectValue && maxBall < selectValue + 20 ) {
+        answerText[1].textContent = "Tabiriklayman siz talabalikka shartnoma asosida qabul qilindingiz "
+    }
+    else if(inputValue >= 0 && inputValue < selectValue || maxBall >= 0 && maxBall < selectValue) {
+        answerText[2].textContent = "Siz talabalikka qabul qilinmadingiz"
+    }
+    else if(inputValue < 0 || maxBall < 0) {
+        answerText[3].textContent = "Iltimos musbat son kiriting"
+    }
+    else if(checkboxValue == 0){
+        answerText[3].textContent = "Iltimos yo'nalishni tanlang"
+    }
+
+    console.log(inputValue);
+    console.log(checkboxValue);
+    console.log(maxBall);
+    console.log(selectValue);
+}) 
+
+elFormbtn.addEventListener("click", function restart() {
+    elFormCheck.checked = false;
+
+    elFormInput.value = "";
+    elFormSelect.value = "Tanlang"
+
+    answerText[0].textContent = ""
+    answerText[1].textContent = ""
+    answerText[2].textContent = ""
+    answerText[3].textContent = ""
+}) 
