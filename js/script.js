5c24e0619938c94caf932bd54c6168b634ebcbda
gnurullayev <nurullayevgayrat73@gmail.com>
2022-08-05 10:03:20 +0500

    js file change

--- js/script.js
+++ js/script.js
@@ -20,19 +20,34 @@ elForm.addEventListener("submit", function(e) {
     var maxBall = inputValue + discount;
 
     if(inputValue >= selectValue + 20 || maxBall >= selectValue + 20) {
-        answerText[0].textContent = "Tabiriklayman siz talabalikka grand asosida qabul qilindingiz "
+        answerText[0].textContent = "Tabiriklayman siz talabalikka grand asosida qabul qilindingiz ";
+        answerText[1].textContent = "";
+        answerText[2].textContent = "";
+        answerText[3].textContent = "";
     }
     else if (inputValue >= selectValue && inputValue < selectValue + 20 || maxBall >= selectValue && maxBall < selectValue + 20 ) {
-        answerText[1].textContent = "Tabiriklayman siz talabalikka shartnoma asosida qabul qilindingiz "
+        answerText[1].textContent = "Tabiriklayman siz talabalikka shartnoma asosida qabul qilindingiz ";
+        answerText[0].textContent = "";
+        answerText[2].textContent = "";
+        answerText[3].textContent = "";
     }
     else if(inputValue >= 0 && inputValue < selectValue || maxBall >= 0 && maxBall < selectValue) {
-        answerText[2].textContent = "Siz talabalikka qabul qilinmadingiz"
+        answerText[2].textContent = "Siz talabalikka qabul qilinmadingiz";
+        answerText[0].textContent = ""
+        answerText[1].textContent = ""
+        answerText[3].textContent = ""
     }
     else if(inputValue < 0 || maxBall < 0) {
-        answerText[3].textContent = "Iltimos musbat son kiriting"
+        answerText[3].textContent = "Iltimos musbat son kiriting";
+        answerText[0].textContent = "";
+        answerText[1].textContent = "";
+        answerText[2].textContent = "";
     }
     else if(checkboxValue == 0){
-        answerText[3].textContent = "Iltimos yo'nalishni tanlang"
+        answerText[3].textContent = "Iltimos yo'nalishni tanlang";
+        answerText[0].textContent = "";
+        answerText[1].textContent = "";
+        answerText[2].textContent = "";
     }
 }) 
 
@@ -40,7 +55,7 @@ elFormbtn.addEventListener("click", function restart() {
     elFormCheck.checked = false;
 
     elFormInput.value = "";
-    elFormSelect.value = "Tanlang"
+    elFormSelect.value = "Tanlang";
 
     answerText[0].textContent = ""
     answerText[1].textContent = ""
